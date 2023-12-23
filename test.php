bc80ca402604b75a9adf5451df3921d502ca7f82
tairat1 <group_2012@hotmail.com>
2023-12-23 16:45:54 +0700

    step btn
    ลูกเล่น ฟอร์ม แบบ step

--- test.php
+++ test.php
@@ -4,21 +4,119 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagination Example</title>
-    <link rel="stylesheet" href="css/_custom.css">
 
+    <link rel="stylesheet" href="css/_custom.css">
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
 </head>
 <body>
- <div id="data-list">
- 
+<!-- Step from -->
+<div class="wrapper">
+    <!-- Head Step -->
+	<div class="header">
+		<ul>
+			<li class="active form_1_progessbar">
+				<div>
+					<p>1</p>
+				</div>
+			</li>
+			<li class="form_2_progessbar">
+				<div>
+					<p>2</p>
+				</div>
+			</li>
+			<li class="form_3_progessbar">
+				<div>
+					<p>3</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+    <!-- End Head Step -->
+	<div class="form_wrap">
+		<div class="form_1 data_info">
+			<h2>Signup Info</h2>
+			<form  method="POST">
+				<div class="form_container">
+					<div class="input_wrap">
+						<label for="email">Email Address</label>
+						<input type="text" name="Email Address" class="input" id="email">
+					</div>
+					<div class="input_wrap">
+						<label for="password">Password</label>
+						<input type="password" name="password" class="input" id="password">
+					</div>
+					<div class="input_wrap">
+						<label for="confirm_password">Confirm Password</label>
+						<input type="password" name="confirm password" class="input" id="confirm_password">
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="form_2 data_info" style="display: none;">
+			<h2>Personal Info</h2>
+			<form>
+				<div class="form_container">
+					<div class="input_wrap">
+						<label for="user_name">User Name</label>
+						<input type="text" name="User Name" class="input" id="user_name">
+					</div>
+					<div class="input_wrap">
+						<label for="first_name">First Name</label>
+						<input type="text" name="First Name" class="input" id="first_name">
+					</div>
+					<div class="input_wrap">
+						<label for="last_name">Last Name</label>
+						<input type="text" name="Last Name" class="input" id="last_name">
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="form_3 data_info" style="display: none;">
+			<h2>Professional Info</h2>
+			<form>
+				<div class="form_container">
+					<div class="input_wrap">
+						<label for="company">Current Company</label>
+						<input type="text" name="Current Company" class="input" id="company">
+					</div>
+					<div class="input_wrap">
+						<label for="experience">Total Experience</label>
+						<input type="text" name="Total Experience" class="input" id="experience">
+					</div>
+					<div class="input_wrap">
+						<label for="designation">Designation</label>
+						<input type="text" name="Designation" class="input" id="designation">
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="btns_wrap">
+		<div class="common_btns form_1_btns">
+			<button type="button" class="btn_next">Next <span class="icon"><ion-icon name="arrow-forward-sharp"></ion-icon></span></button>
+		</div>
+		<div class="common_btns form_2_btns" style="display: none;">
+			<button type="button" class="btn_back"><span class="icon"><ion-icon name="arrow-back-sharp"></ion-icon></span>Back</button>
+			<button type="button" class="btn_next">Next <span class="icon"><ion-icon name="arrow-forward-sharp"></ion-icon></span></button>
+		</div>
+		<div class="common_btns form_3_btns" style="display: none;">
+			<button type="button" class="btn_back"><span class="icon"><ion-icon name="arrow-back-sharp"></ion-icon></span>Back</button>
+			<button type="button" class="btn_done">Done</button>
+		</div>
+	</div>
+</div>
 
- </div> <br>
-<div id="pagination" ></div>
+<div class="modal_wrapper">
+	<div class="shadow"></div>
+	<div class="success_wrap">
+		<span class="modal_icon"><ion-icon name="checkmark-sharp"></ion-icon></span>
+		<p style="color: #00f4a7f4;">You have successfully completed the process.</p>
+	</div>
+</div>
+<!-- End Step from -->
 
-
-
-<script src="jsd.min.js">
-
-</script>
+<script src="js/step_btn.js"></script>
 
 </body>
 </html>
